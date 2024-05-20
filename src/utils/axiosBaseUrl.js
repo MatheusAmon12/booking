a8dfe94a88277fae7f4adb69af3fc109339d4beb
Matheus Amon <amonmatheus757@gmail.com>
2024-05-20 14:56:49 -0300

    feat: update api url

--- src/utils/axiosBaseUrl.js
+++ src/utils/axiosBaseUrl.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const baseURL = () => axios.create({
-    // utilizar esta abordagem apenas para desenvolvimento
-    //baseURL: "http://localhost:10000/api/",
     baseURL: "https://booking-api-sdsh.onrender.com/api/",
 })
