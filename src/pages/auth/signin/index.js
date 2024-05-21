485f38352111bc227a92aa1710919b0887758ca7
Matheus Amon <amonmatheus757@gmail.com>
2024-05-21 17:08:19 -0300

    chore: add log to sign in

--- src/pages/auth/signin/index.js
+++ src/pages/auth/signin/index.js
@@ -56,6 +56,7 @@ const Signin = () => {
         await signIn('google', {
             callbackUrl: process.env.NEXT_PUBLIC_SITE_URL,
         })
+        console.log(process.env.NEXT_PUBLIC_SITE_URL)
     }
 
     return(
