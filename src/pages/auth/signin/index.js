fe053b9b54af1aabaa0b06eeb8a98ceaf14e122f
Matheus Amon <amonmatheus757@gmail.com>
2024-05-21 16:50:25 -0300

    fix: remove template string to url callbacks

--- src/pages/auth/signin/index.js
+++ src/pages/auth/signin/index.js
@@ -48,13 +48,13 @@ const Signin = () => {
         await signIn('credentials', {
             email: values.email,
             password: values.password,
-            callbackUrl: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+            callbackUrl: process.env.NEXT_PUBLIC_SITE_URL,
         })
             .then(res => console.log(res))
     }
     const handleClickGoogle = async () => {
         await signIn('google', {
-            callbackUrl: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+            callbackUrl: process.env.NEXT_PUBLIC_SITE_URL,
         })
     }
 
